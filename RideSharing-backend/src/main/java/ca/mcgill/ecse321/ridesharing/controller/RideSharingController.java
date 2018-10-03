44b5dd1f044e240ede5966697ea72e0a3843b13d
Benjamin Szwimer <benji.szwimer@gmail.com>
2018-10-02 22:24:43 -0400

    controller written for get and post of passenger

--- RideSharing-backend/src/main/java/ca/mcgill/ecse321/ridesharing/controller/RideSharingController.java
+++ RideSharing-backend/src/main/java/ca/mcgill/ecse321/ridesharing/controller/RideSharingController.java
@@ -1,5 +1,39 @@
 package ca.mcgill.ecse321.ridesharing.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.mcgill.ecse321.ridesharing.model.*;
+import ca.mcgill.ecse321.ridesharing.repository.RideSharingRepository;
+
+@RestController
 public class RideSharingController {
+
+	@Autowired
+	RideSharingRepository repository;
+
+	@RequestMapping("/")
+	public String greeting() {
+		return "Welcome to the ride sharing app!";
+	}
+
+	@PostMapping("/passenger/{username}/{password}")
+	public String createParticipant(@PathVariable("username") String username, @PathVariable ("password") String password) {
+		Passenger passenger = repository.createPassenger(username, password);
+		return passenger.getUsername();
+	}
+
+	@GetMapping("/passenger/{username}")
+	public String getPassenger(@PathVariable("username") String username) {
+		Passenger passenger = repository.getPassenger(username);
+		if(passenger == null) {
+			return "NOT FOUND";
+		}
+		return passenger.getUsername();
+	}
 
 }

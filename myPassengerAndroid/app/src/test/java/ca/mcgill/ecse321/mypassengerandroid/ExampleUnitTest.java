db875c049410ef0a8e4218fcbb0dd21057661e5f
ethan <ethan.itovitch@mail.mcgill.ca>
2018-11-02 11:38:08 -0400

    unit tests for passenger complete

--- myPassengerAndroid/app/src/test/java/ca/mcgill/ecse321/mypassengerandroid/ExampleUnitTest.java
+++ myPassengerAndroid/app/src/test/java/ca/mcgill/ecse321/mypassengerandroid/ExampleUnitTest.java
@@ -1,17 +1,198 @@
 package ca.mcgill.ecse321.mypassengerandroid;
 
-import org.junit.Test;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+import cz.msebera.android.httpclient.Header;
+
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+
+    long id;
+
+    @Before
+    public void setup() {
+        HttpUtils.post("signIn/" + "aaa" + "/" + "zzz", new RequestParams(), new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+
+            }
+
+        });
+
+        HttpUtils.post("signIn/" + "driver" + "/" + "zzz", new RequestParams(), new JsonHttpResponseHandler() {
+
+        });
+
+        HttpUtils.post("createRoute/4/Mtl/Tro/1-1-2018/lambo/driver/100", new RequestParams(), new JsonHttpResponseHandler() {
+
+        });
+
     }
+
+    @Test
+    public void findRouteCreateRouteCheck() {
+
+        HttpUtils.get("findRoute/" + "1-1-2018" + "/" + "Mtl" + "/" + "Tro", new RequestParams(), new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                super.onSuccess(statusCode, headers, response);
+                String vehicle = "";
+                String price = "";
+                String list = "";
+                for (int i = 0; i < response.length(); i++) {
+
+                    JSONObject jsonobject = null;
+                    try {
+                        jsonobject = response.getJSONObject(i);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        vehicle = jsonobject.getString("vehicle");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        price = jsonobject.getString("price");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        id = jsonobject.getLong("id");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    assertNotNull(jsonobject);
+                    assertEquals(vehicle, "lambo");
+                    assertEquals(price, "100");
+
+
+                }
+
+            }
+
+        });
+
+
+    }
+
+    @Test
+    public void joinRouteShowPassengersRouteCheck() {
+
+        HttpUtils.get("joinRoute/" + id + "/" + "aaa", new RequestParams(), new JsonHttpResponseHandler() {
+
+        });
+
+        HttpUtils.get("showPassengersRoutes/" + "aaa", new RequestParams(), new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                super.onSuccess(statusCode, headers, response);
+
+                long foundID = 0;
+
+
+                for (int i = 0; i < response.length(); i++) {
+                    JSONObject jsonobject = null;
+                    try {
+                        jsonobject = response.getJSONObject(i);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+
+                        foundID = jsonobject.getLong("id");
+
+                    } catch (JSONException e) {
+
+
+                    }
+
+                }
+
+                assertEquals(foundID, id);
+
+            }
+
+
+        });
+
+    }
+
+    @Test
+    public void leaveRouteCheck() {
+
+        HttpUtils.get("joinRoute/" + id + "/" + "aaa", new RequestParams(), new JsonHttpResponseHandler() {
+
+        });
+
+        HttpUtils.get("showPassengersRoutes/" + "aaa", new RequestParams(), new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                super.onSuccess(statusCode, headers, response);
+
+                long foundID = 0;
+
+
+                for (int i = 0; i < response.length(); i++) {
+                    JSONObject jsonobject = null;
+                    try {
+                        jsonobject = response.getJSONObject(i);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+
+                        foundID = jsonobject.getLong("id");
+
+                    } catch (JSONException e) {
+
+
+                    }
+
+                }
+
+                assertEquals(foundID,0);
+
+            }
+
+
+        });
+
+    }
+
+    @After
+    public void cleanUp() {
+
+        HttpUtils.get("endRoute/" + id, new RequestParams(), new JsonHttpResponseHandler() {
+
+        });
+
+    }
+
 }

0c9851aff294ee7193a75950f1e69b69d7d9d96f
Benjamin Szwimer <benji.szwimer@gmail.com>
2018-11-02 02:27:19 -0400

    changing passenger

--- myPassengerAndroid/app/src/main/java/ca/mcgill/ecse321/mypassengerandroid/DatePickerFragment.java
+++ myPassengerAndroid/app/src/main/java/ca/mcgill/ecse321/mypassengerandroid/DatePickerFragment.java
@@ -2,36 +2,37 @@ package ca.mcgill.ecse321.mypassengerandroid;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
-import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.widget.DatePicker;
 
-public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+import java.util.Calendar;
 
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current date as the default date in the picker
-            final Calendar c = Calendar.getInstance();
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
 
-            // Parse the existing time from the arguments
-            Bundle args = getArguments();
-            if (args != null) {
-                year = args.getInt("year");
-                month = args.getInt("month");
-                day = args.getInt("day");
-            }
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
 
-            // Create a new instance of DatePickerDialog and return it
-            return new DatePickerDialog(getActivity(), this, year, month, day);
+        // Parse the existing time from the arguments
+        Bundle args = getArguments();
+        if (args != null) {
+            year = args.getInt("year");
+            month = args.getInt("month");
+            day = args.getInt("day");
         }
 
-        public void onDateSet(DatePicker view, int year, int month, int day) {
-            MainActivity myActivity = (MainActivity)getActivity();
-            myActivity.setDate(getArguments().getInt("id"), day, month, year);
-        }
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        FindRoute myActivity = (FindRoute) getActivity();
+        myActivity.setDate(getArguments().getInt("id"), day, month, year);
     }
 }
--- myPassengerAndroid/app/src/main/java/ca/mcgill/ecse321/mypassengerandroid/FindRoute.java
+++ myPassengerAndroid/app/src/main/java/ca/mcgill/ecse321/mypassengerandroid/FindRoute.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class FindRoute extends AppCompatActivity {
 
@@ -25,5 +26,44 @@ public class FindRoute extends AppCompatActivity {
             }
         });
     }
+
+
+    private Bundle getDateFromLabel(String text) {
+        Bundle rtn = new Bundle();
+        String comps[] = text.toString().split("-");
+        int day = 1;
+        int month = 1;
+        int year = 1;
+
+        if (comps.length == 3) {
+            day = Integer.parseInt(comps[0]);
+            month = Integer.parseInt(comps[1]);
+            year = Integer.parseInt(comps[2]);
+        }
+
+        rtn.putInt("day", day);
+        rtn.putInt("month", month-1);
+        rtn.putInt("year", year);
+
+        return rtn;
+    }
+
+
+    public void showDatePickerDialog(View v) {
+        TextView tf = (TextView) v;
+        Bundle args = getDateFromLabel(tf.getText().toString());
+        args.putInt("id", v.getId());
+
+        DatePickerFragment newFragment = new DatePickerFragment();
+        newFragment.setArguments(args);
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+
+    public void setDate(int id, int d, int m, int y) {
+        TextView tv = (TextView) findViewById(id);
+        tv.setText(String.format("%02d-%02d-%04d", d, m + 1, y));
+    }
+
 
 }
